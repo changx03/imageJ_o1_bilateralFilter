e6f987b791f1145f8cc144b06bae6acddd3d35f8
Luke Chang <luke.x.chang@gmail.com>
2015-11-28 00:24:31 +1300

    UI added

--- src/main/java/filterPlugin_.java
+++ src/main/java/filterPlugin_.java
@@ -2,57 +2,104 @@
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import ij.plugin.filter.PlugInFilter;
 import ij.process.ByteProcessor;
 import ij.process.FloatProcessor;
 import ij.process.ImageProcessor;
-import java.awt.Point;
 import java.awt.Rectangle;
 
 public class filterPlugin_ implements PlugInFilter {
 
-    private ImagePlus imp;
+    // image properties
+    private ImagePlus image;
+    private String imageTitle;
+    private Rectangle rec;
+
+    // parameters from dialog
+    private final String[] filterChoices = {"Integral arithmetic mean", "Integral triangular filter", "Integral polynomial filter", "Convolutional polynomial filter"};
+
+    private enum Filters {
+
+        INT_MEAN, INT_TRI, INT_POLY, CON_POLY
+    };
+    private int radius;
+    private Filters filter;
 
     @Override
     public int setup(String string, ImagePlus ip) {
-        this.imp = imp;
-        return IJ.setupDialog(imp, DOES_8G + DOES_16 + SUPPORTS_MASKING);
+        this.image = ip;
+        imageTitle = image.getShortTitle();
+        if (!showDialog()) {
+            return DONE;
+        }
+        return IJ.setupDialog(image, DOES_8G + DOES_16 + SUPPORTS_MASKING);
+    }
+
+    private boolean showDialog() {
+        GenericDialog gd = new GenericDialog("Filters");
+        gd.addChoice("Filter", filterChoices, filterChoices[0]);
+        gd.addNumericField("window radius", 2, 0);
+
+        gd.showDialog();
+        if (gd.wasCanceled()) {
+            return false;
+        }
+        // get entered values
+        String selectedChoice = gd.getNextChoice();
+        if (selectedChoice.equals(filterChoices[0])) {
+            filter = Filters.INT_MEAN;
+        } else if (selectedChoice.equals(filterChoices[1])) {
+            filter = Filters.INT_TRI;
+        } else if (selectedChoice.equals(filterChoices[2])) {
+            filter = Filters.INT_POLY;
+        } else if (selectedChoice.equals(filterChoices[3])) {
+            filter = Filters.CON_POLY;
+        }
+        radius = (int) gd.getNextNumber();
+        return true;
     }
 
     @Override
     public void run(ImageProcessor ip) {
         FloatProcessor fp = null;
         fp = ip.toFloat(0, fp);
-        Rectangle rec = ip.getRoi();
+        rec = ip.getRoi();
         float[] pixels = (float[]) fp.getPixels();  // oringal image
         IntegralImage integral = new IntegralImage(pixels, rec);// getIntegralImg image
-
+        
         // display getIntegralImg image in new window
 //        show32bitImage(integral.integralImg, rec, "IntegralSum");
 //        integral.initialIntegralImgs4Polynomial();
 //        show32bitImage(integral.zIntegral, rec, "zIntegral");
 //        show32bitImage(integral.z2Integral, rec, "z2Integral");
-
-        // integral arithmetic mean filter
-//        int r = 5;
-//        SpatialFilters sf = new SpatialFilters(integral);
-//        double[] meanImg = sf.integralMean(r);
-//        show8bitImage(meanImg, rec, "integralMeanR" + r);
         
-        // triangular filter
-//        int radius = 5;
-//        SpatialFilters sf = new SpatialFilters(integral);
-//        double[] triSmoothImg = sf.integralTriangular(radius);
-//        show8bitImage(triSmoothImg, rec, "integralTriangleR" + radius);
-        
-        // polynomial filter
-        int radius = 2;
+        String outputImageTitle;
+        double[] outImg;
         SpatialFilters sf = new SpatialFilters(integral);
-        double[] convoPoly = sf.convolutionalPolynomial(radius);
-        show8bitImage(convoPoly, rec, "convolutionalPolySmoothR" + radius);
-        
-        double[] polySmoothImg = sf.integralPolynomial(radius);
-        show8bitImage(polySmoothImg, rec, "integralPolySmoothR" + radius);
+        switch (filter) {
+            case INT_MEAN:  // integral arithmetic mean filter
+                outImg = sf.integralMean(radius);
+                outputImageTitle = imageTitle + "_integralMeanR" + radius;
+                break;
+            case INT_TRI:   // triangular filter
+                outImg = sf.integralTriangular(radius);
+                outputImageTitle = imageTitle + "_integralTriR" + radius;
+                break;
+            case INT_POLY:  // polynomial filter
+                outImg = sf.integralPolynomial(radius);
+                outputImageTitle = imageTitle + "_integralPolyR" + radius;
+                break;
+            case CON_POLY:
+                outImg = sf.convolutionalPolynomial(radius);
+                outputImageTitle = imageTitle + "_convoPolyR" + radius;
+                break;
+            default:
+                outImg = new double[rec.width * rec.height];
+                outputImageTitle = imageTitle;
+                break;
+        }
+        show8bitImage(outImg, rec, outputImageTitle);
     }
 
     private void show8bitImage(double[] image, Rectangle rec, String imageTitle) {
